99cfb652816cbfbbef91bb4a3257a6f49ae0a5b7
hideto kashiyama <kwes@nifty.com>
2018-12-05 00:50:26 +0000

    181205-3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+    
+    def bootstrap_class_for(flash_type)
+    case flash_type
+      when :success
+        "alert-success"
+      when :error
+        "alert-danger"
+      when :alert
+        "alert-warning"
+      when :notice
+        "alert-info"
+      else
+        flash_type.to_s
+    end
+  end
+  
+  def full_title(page_title)
+    base_title = "Yoyaku"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
+  
 end
